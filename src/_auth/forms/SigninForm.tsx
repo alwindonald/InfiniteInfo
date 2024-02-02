ba34d618d84cf175caba2162cf7e1c3bf0e42679
alwindonald <alwindonald4004@gmail.com>
2024-02-02 21:49:20 +0530

    sixth commit

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,27 +1,25 @@
 import * as z from "zod";
 import { useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router-dom";
-import { useToast } from "@/components/ui/use-toast";
-
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Link, useNavigate } from "react-router-dom";
 
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Loader from "@/components/shared/Loader";
+import { useToast } from "@/components/ui/use-toast";
 
 import { SigninValidation } from "@/lib/validation";
 import { useSignInAccount } from "@/lib/react-query/queriesAndMutations";
 import { useUserContext } from "@/context/AuthContext";
 
-
 const SigninForm = () => {
-  const { toast } = useToast()
-  const { checkAuthUser , isLoading: isUserLoading } = useUserContext();
+  const { toast } = useToast();
   const navigate = useNavigate();
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
 
- 
-  const { mutateAsync: signInAccount} = useSignInAccount();
+  // Query
+  const { mutateAsync: signInAccount, isPending } = useSignInAccount();
 
   const form = useForm<z.infer<typeof SigninValidation>>({
     resolver: zodResolver(SigninValidation),
@@ -29,53 +27,44 @@ const SigninForm = () => {
       email: "",
       password: "",
     },
-  })
+  });
 
+  const handleSignin = async (user: z.infer<typeof SigninValidation>) => {
+    const session = await signInAccount(user);
 
-
-     // 2. Define a submit handler.
-  async function onSubmit(values: z.infer<typeof SigninValidation>) {
-
-    const session = await signInAccount({
-      email: values.email,
-        password: values.password,
-    })
-
-
-    if(!session) {
-      return toast({ title: ' Sign up failed. Please try again.'})
+    if (!session) {
+      toast({ title: "Login failed. Please try again." });
+      
+      return;
     }
 
     const isLoggedIn = await checkAuthUser();
 
-
-    if(isLoggedIn){
+    if (isLoggedIn) {
       form.reset();
 
-      navigate('/')
-    }else {
-      return toast({ title: ' Sign up failed. Please try again.'})
+      navigate("/");
+    } else {
+      toast({ title: "Login failed. Please try again.", });
+      
+      return;
     }
-  }
+  };
 
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="logo" />
 
-return (
-<div>
-  <Form {...form}>
-    <div className="sm:w-420 flex-center flex-col">
-    <img src="/assets/images/logo.svg" alt="logo" />
-
-    <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
-      Log in to your account
-    </h2>
-    
-    <p className="text-light-3 small-medium md:base-regular mt-2">
-     Welcome back! Please enter your details
-    </p>
-
-    </div>
-
-    <form  onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4"> 
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Log in to your account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          Welcome back! Please enter your details.
+        </p>
+        <form
+          onSubmit={form.handleSubmit(handleSignin)}
+          className="flex flex-col gap-5 w-full mt-4">
           <FormField
             control={form.control}
             name="email"
@@ -103,31 +92,29 @@ return (
               </FormItem>
             )}
           />
-      <Button type="submit" className="shad-button_primary">
-      {isUserLoading ? (
+
+          <Button type="submit" className="shad-button_primary">
+            {isPending || isUserLoading ? (
               <div className="flex-center gap-2">
                 <Loader /> Loading...
               </div>
             ) : (
-              "Sign in"
+              "Log in"
             )}
-      </Button>
-    </form>
+          </Button>
 
-    <p className="text-small-regular text-light-2 text-center mt-2">
-        Don't have an account?
-       <Link
-         to="/sign-up"
-         className="text-primary-500 text-small-semibold ml-1">
-         Sign up
-      </Link>
-    </p>
-  </Form>
-</div>
-)
-}
-      
-export default SigninForm
-     
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Don&apos;t have an account?
+            <Link
+              to="/sign-up"
+              className="text-primary-500 text-small-semibold ml-1">
+              Sign up
+            </Link>
+          </p>
+        </form>
+      </div>
+    </Form>
+  );
+};
 
-    
+export default SigninForm;
